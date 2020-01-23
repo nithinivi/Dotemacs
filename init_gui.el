1a379b33b36dfe16f3096baa1ee918bff54ba647
nithinivi <nithinnivi@gmail.com>
2020-01-23 11:52:29 +0530

    use-package g-mode edits <error> flymake mode

--- init_gui.el
+++ init_gui.el
@@ -91,29 +91,22 @@
 (defvar go-use-gometalinter nil
   "Use gometalinter if the variable has non-nil value.")
 
-
-(defun my-go-mode-hook ()
-																				; Use goimports instead of go-fmt	
-	(if (executable-find "goimports")
-	    (setq gofmt-command "goimports"))
-																				; Customize compile command to run go build
-  (if (not (string-match "go" compile-command))
-      (set (make-local-variable 'compile-command)
-           "go build -v && go test -v && go vet"))
-																				; Godef jump key binding
- 	)
-
+(defun my-go-run-main ()
+  (interactive)
+  (shell-command
+   (format "go run %s"
+	   (shell-quote-argument (buffer-file-name)))))
 
 
 (defun spacemacs//go-enable-gometalinter ()
   "Enable `flycheck-gometalinter' and disable overlapping `flycheck' linters."
   (setq flycheck-disabled-checkers '(go-gofmt
-																		 go-golint
-																		 go-vet
-																		 go-build
-																		 go-test
-																		 go-errcheck)
-				))
+				     go-golint
+				     go-vet
+				     go-build
+				     go-test
+				     go-errcheck)
+	))
 
 (defun my-go-var-shortcut ()
   (interactive)
@@ -122,163 +115,81 @@
 (use-package go-mode
   :ensure t
   :init	(progn
-					(autoload 'go-mode "go-mode" nil t)
-					(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
-					(setq-local tab-width go-tab-width))
-	:hook
-	((go-mode-hook . my-go-mode-hook)
-	 (go-mode-hook . flycheck-mode)
-	 (before-save-hook . gofmt-before-save )
-	 )
-	:bind	(:map go-mode-map
-							("C-;" . my-go-var-shortcut)
-							("M-." . godef-jump)
-							("M-*" . pop-tag-mark)
-							("<C-return>" . compile)
+	  (autoload 'go-mode "go-mode" nil t)
+	  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+	  (setq-local tab-width go-tab-width)
+	  )
 
-							)
-	:config
-	(flymake-mode -1)
-	)
+
+  :config (progn
+	    (flymake-mode -1)
+	    (if  (string-match "make" compile-command)
+		(set (make-local-variable 'compile-command)
+		     "go build -v && go test -v && go vet"))
+	    (if (executable-find "goimports")
+		(setq gofmt-command "goimports")))
+
+  :hook ((go-mode-hook . flycheck-mode)
+	 (before-save-hook . gofmt-before-save ))
+
+  :bind	(:map go-mode-map
+	      ("C-;" . my-go-var-shortcut)
+	      ("M-." . godef-jump)
+	      ("M-*" . pop-tag-mark)
+	      ("C-c RET" . compile)
+	      ("<C-return>" . my-go-run-main))  
+  )
 
 
 
 (use-package flycheck-gometalinter
-	:defer t
-	:init
-	(add-hook 'go-mode-hook 'spacemacs//go-enable-gometalinter t))
+  :defer t
+  :init
+  (add-hook 'go-mode-hook 'spacemacs//go-enable-gometalinter t))
 
 
 (use-package go-complete
   :ensure t
-	:hook
-	(completion-at-point-functions . go-complete-at-point)
-	)
+  :hook
+  (completion-at-point-functions . go-complete-at-point)
+  )
 
 (use-package go-guru
-	:ensure t
-	:hook
-	(go-mode-hook . #'go-guru-hl-identifier-mode)
-	)
+  :ensure t
+  :hook
+  (go-mode-hook . #'go-guru-hl-identifier-mode)
+  )
 
 
 
-	;; (use-package gorepl-mode
-	;;   :ensure t
-	;;   :config
-	;;   (add-hook 'go-mode-hook #'gorepl-mode)
-	;;   )
+(use-package gorepl-mode
+  :ensure t
+  :hook
+  (go-mode-hook . #'gorepl-mode)
+  )
 
-	;; (use-package go-playground
-	;;   :ensure t
-	;;   )
+(use-package go-playground
+  :ensure t
+  )
 
-	;; (defun set-exec-path-from-shell-PATH ()
-	;;   (let ((path-from-shell (replace-regexp-in-string
-	;;                           "[ \t\n]*$"
-	;;                           ""
-	;;                           (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-	;;     (setenv "PATH" path-from-shell)
-	;;     (setq eshell-path-env path-from-shell) ; for eshell users
-	;;     (setq exec-path (split-string path-from-shell path-separator)))
-	;;   )
+;;web-mode
+;;============================================================================
 
-	;; (when window-system (set-exec-path-from-shell-PATH))
-	;; (setenv "GOPATH" "/home/nithin/go_code/")
-	;; (add-to-list 'exec-path "/home/nithin/go_code/bin")
+(use-package web-mode
+  :ensure t
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.api\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("/some/react/path/.*\\.js[x]?\\'" . web-mode))
 
+  (setq web-mode-content-types-alist
+	'(("json" . "/some/path/.*\\.api\\'")
+	  ("xml"  . "/other/path/.*\\.api\\'")
+	  ("jsx"  . "/some/react/path/.*\\.js[x]?\\'")))
 
-	;; ;; (defun auto-complete-for-go ()
-	;; ;;   (auto-complete-mode 1)
-	;; ;;   (flymake-mode 1)
-	;; ;;   (flycheck-mode 1)
-	;; ;;   (yas-minor-mode 1)
-	;; ;;   )
-	;; ;; (add-hook 'go-mode-hook 'auto-complete-for-go)
-	;; (use-package go-guru
-	;;   :ensure t
-	;;   :config
-	;;   (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
-	;;   )
-
-	;; (use-package go-autocomplete
-	;;   :ensure t
-	;;   :config
-	;;   (with-eval-after-load 'go-mode
-	;;     (require 'go-autocomplete)
-	;;     )
-	;;   )
-
-
-	;; (defun my-go-var-shortcut ()
-	;;   (interactive)
-	;;   (insert " := "))
-
-	;; (defun my-go-mode-hook ()
-	;;                                         ; Use goimports instead of go-fmt
-	;;   (setq gofmt-command "goimports")
-	;;                                         ; Call Gofmt before saving
-	;;   (add-hook 'before-save-hook 'gofmt-before-save)
-	;;                                         ; Customize compile command to run go build
-	;;   (if (not (string-match "go" compile-command))
-	;;       (set (make-local-variable 'compile-command)
-	;;            "go build -v && go test -v && go vet"))
-	;;                                         ; Godef jump key binding
-
-	;;   (local-set-key (kbd "M-.") 'godef-jump)
-	;;   (local-set-key (kbd "M-*") 'pop-tag-mark)
-	;;   (local-set-key (kbd "C-;") 'my-go-var-shortcut)
-	;;   (local-set-key (kbd "<C-return>") 'compile)
-	;;   )
-
-
-	;; (add-hook 'go-mode-hook 'my-go-mode-hook)
-
-	;; ;; (defun my-go-mode-hook ()
-	;; ;; 					; Use goimports instead of go-fmt
-	;; ;;   (setq gofmt-command "goimports")
-	;; ;; 					; Call Gofmt before saving
-	;; ;;   (add-hook 'before-save-hook 'gofmt-before-save)
-	;; ;; 					; Customize compile command to run go build
-	;; ;;   (if (not (string-match "go" compile-command))
-	;; ;;       (set (make-local-variable 'compile-command)
-	;; ;;            "go build -v && go test -v && go vet"))
-	;; ;; 					; Godef jump key binding
-	;; ;;   (local-set-key (kbd "M-.") 'godef-jump)
-	;; ;;   (local-set-key (kbd "M-*") 'pop-tag-mark)
-	;; ;;   )
-	;; ;; (add-hook 'go-mode-hook 'my-go-mode-hook)
-
-	;; ;; company mode support
-
-	;; (use-package company-go
-	;;   :ensure t)
-
-	;; (add-hook 'go-mode-hook
-	;;           (lambda ()
-	;;             (set (make-local-variable 'company-backends) '(company-go))
-	;;             (company-mode)))
-
-
-
-	;;web-mode
-	;;============================================================================
-
-	(use-package web-mode
-		:ensure t
-		:config
-		(setq web-mode-markup-indent-offset 2)
-		(setq web-mode-css-indent-offset 2)
-		(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-		(add-to-list 'auto-mode-alist '("\\.api\\'" . web-mode))
-		(add-to-list 'auto-mode-alist '("/some/react/path/.*\\.js[x]?\\'" . web-mode))
-
-		(setq web-mode-content-types-alist
-					'(("json" . "/some/path/.*\\.api\\'")
-						("xml"  . "/other/path/.*\\.api\\'")
-						("jsx"  . "/some/react/path/.*\\.js[x]?\\'")))
-
-		)
+  )
 (use-package emmet-mode
   :ensure t
   :hook (html-mode . emmet-mode))
@@ -336,7 +247,7 @@
                  (sql-user "nithin")
                  (sql-database "kyc"))
         )
-			)
+      )
 
 (defun my-sql-connect (product connection)
   ;; load the password
@@ -353,7 +264,7 @@
   ;; connect to database
   (setq sql-product product)
   (sql-connect connection)
-	)
+  )
 
 
 ;; Projectile mode
@@ -408,8 +319,8 @@
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
-				 ("\\.md\\'" . markdown-mode)
-				 ("\\.markdown\\'" . markdown-mode))
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
 ;; JS mode
@@ -439,7 +350,7 @@
   (define-key js-mode-map (kbd "M-.") nil)
 
   (add-hook 'js2-mode-hook (lambda ()
-														 (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
+			     (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
 
 
 
@@ -460,8 +371,8 @@
 
 (add-to-list 'company-backends 'company-tern)
 (add-hook 'js2-mode-hook (lambda ()
-													 (tern-mode)
-													 (company-mode)))
+			   (tern-mode)
+			   (company-mode)))
 
 (define-key tern-mode-keymap (kbd "M-.") nil)
 (define-key tern-mode-keymap (kbd "M-,") nil)
